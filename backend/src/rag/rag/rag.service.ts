c8226027b0c940059899b5a5bd12f1d7d385d021
HardMachinesCorporation <thejordach@gmail.com>
2025-02-22 23:21:05 +0000

    adding rag service

--- backend/src/rag/rag/rag.service.ts
+++ backend/src/rag/rag/rag.service.ts
@@ -1,4 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import { END, MemorySaver, START, StateGraph} from '@langchain/langgraph';
+import { OllamaService } from 'src/ollama/ollama.service';
+import { hasSubscribers } from 'diagnostics_channel';
+import {StringOutputParser} from "@langchain/core/output_parsers"
+import { pull } from "langchain/hub";
+import { AnyAaaaRecord } from 'dns';
+
+
+interface RAGState {
+    question: string;
+    generation: string;
+    document: Document[];
+    channels: any;
+   
+}
+
 
 @Injectable()
-export class RagService {}
+export class RagService {
+    ragChain:any
+    graph:any 
+
+    constructor(
+        private readonly ollamaService: OllamaService,
+    ){
+        // Initialize the ragChain asynchronously
+        this.initializeRagChain();
+    }
+
+
+    private async initializeRagChain() {
+        try {
+            // Retrieve the prompt module
+            const module = await pull('sde/rag-prompot');
+            // Set up the ragChain
+            this.ragChain = module.pipe(this.ollamaService.chat).pipe(new StringOutputParser());
+        } catch (error) {
+            console.error('Error initializing ragChain:', error);
+        }
+    }
+
+
+    async run(question:string): Promise<string>{
+        const app = this.graph.compile({
+            checkpointer:MemorySaver,
+        })
+        return app.invoke(question)
+    }
+
+    async setupGraph(_graphState:RAGState){
+        this.graph = new StateGraph({
+            channels:_graphState
+        })
+    }
+}
